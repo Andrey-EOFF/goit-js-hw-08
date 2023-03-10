fbc4d28b1d7400941418692be230705df5e34cf1
Andrey-EOFF <113987678+Andrey-EOFF@users.noreply.github.com>
2023-03-10 18:15:17 +0200

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+
+const items = galleryItems
+  .map(item => {
+    return `
+    <a class="gallery__item" href="${item.original}" >
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>`;
+  })
+  .join('');
+
+const galleryBox = document.querySelector('.gallery');
+galleryBox.innerHTML = items;
+
+new SimpleLightbox('.gallery a', {
+  caption: true,
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  swipeClose: true,
+  animationSpeed: 300,
+});
 
 console.log(galleryItems);
